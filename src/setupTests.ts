d3ebd7744c9471cafc37982b6b3e54aa7711d6a1
yuuko404 <yoxixi233@gmail.com>
2022-05-20 12:15:49 +0900

    first commit

---
+++ src/setupTests.ts
@@ -0,0 +1,7 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+// 单元测试
+// public文件夹不参与打包
+import "@testing-library/jest-dom";
